ef1bd6d079ddf9d4f6d53ce5681dd9e837aad81d
Aaron Johnson <aaron@aaronjohnson.xyz>
2026-09-04 07:13:24 -0400

    Custom config.h and GCC compat fixes for termhandler/delim

--- config.h
+++ config.h
@@ -1,36 +1,20 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+/* dwmblocks config.h — USG Engineering Theme
+ * Luke's dwmblocks format: icon, command, interval, signal
+ * signal numbers are SIGRTMIN+N, keep N between 1-15
+ */
+
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "sb-kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-tasks",	10,	26},
-	/* {"",	"sb-music",	0,	11}, */
-	{"",	"sb-pacpackages",	0,	8},
-	{"",	"sb-news",		0,	6},
-	/* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25}, */
-	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸 23",			9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰 21",			9000,	21}, */
-	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
-	/* {"",	"sb-moonphase",	18000,	17}, */
-	{"",	"sb-doppler",	0,	13},
-	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
-	{"",	"sb-nettraf",	1,	16},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
-	{"",	"sb-internet",	5,	4},
-	/* {"",	"sb-iplocate", 0,	27}, */
-	{"",	"sb-help-icon",	0,	15},
+	/* icon    command          interval   signal */
+	{ "",     "sb-music",          5,         1 },
+	{ "",     "sb-cpu",            2,         2 },
+	{ "",     "sb-memory",         2,         3 },
+	{ "",     "sb-disk",          60,         4 },
+	{ "",     "sb-ip",           300,         5 },
+	{ "",     "sb-uptime",        60,         6 },
+	{ "",     "sb-temp",          10,         7 },
+	{ "",     "sb-volume",         0,         8 },
+	{ "",     "sb-datetime",      30,         9 },
 };
 
-//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+static char *delim = "  ";
+static unsigned int delimLen = 2;
